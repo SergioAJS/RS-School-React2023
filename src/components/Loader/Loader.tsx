1720274909c281cba6049bf4220dbc4c85ce9297
SergioAJS <SergFanMadrid@gmail.com>
2023-04-04 15:02:28 +0500

    feat: change loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,10 +1,10 @@
 import loader from '../../resources/loading.svg';
 import styles from './Loader.module.scss';
 
-export function Loader() {
+export const Loader = () => {
   return (
     <div className={styles.loader__container}>
       <img className={styles.loader} src={loader} alt="loading" />
     </div>
   );
-}
+};
