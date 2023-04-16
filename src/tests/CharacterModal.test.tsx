b61707213107a2d490fbd00c0e75c949a99bfd1f
SergioAJS <SergFanMadrid@gmail.com>
2023-04-17 00:08:38 +0500

    refactor: delete comments

--- src/tests/CharacterModal.test.tsx
+++ src/tests/CharacterModal.test.tsx
@@ -10,9 +10,6 @@ const mockModalClick = vi.fn();
 describe('Card modal', () => {
   it('Renders card modal', () => {
     renderWithProviders(<CardModal onClose={mockModalClick} />);
-    // expect(screen.getByRole('heading', { level: 3 })).toHaveTextContent('Rick Sanchez');
     expect(screen.getByAltText('loading')).toBeInTheDocument();
-    // expect(screen.getByAltText('Rick Sanchez')).toBeInTheDocument();
-    // expect(screen.getByText(/Gender/i)).toHaveTextContent('Gender: Male');
   });
 });
